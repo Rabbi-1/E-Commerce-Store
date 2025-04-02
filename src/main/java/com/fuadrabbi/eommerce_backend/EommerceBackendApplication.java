5f6bc5a07c8c44cb978337a47f4e21b4ef41894a
Fuad Rabbi <fuadhrabbi@gmail.com>
2025-04-02 14:42:51 -0400

    Test

---
+++ src/main/java/com/fuadrabbi/eommerce_backend/EommerceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fuadrabbi.eommerce_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EommerceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EommerceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fuadrabbi/eommerce_backend/EommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fuadrabbi.eommerce_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
