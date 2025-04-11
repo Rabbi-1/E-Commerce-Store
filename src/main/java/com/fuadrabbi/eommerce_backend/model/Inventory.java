0632ff15375532a9ec12dd540c4da59ce8e4f0b1
Fuad Rabbi <fuadhrabbi@gmail.com>
2025-04-10 21:39:51 -0400

    Created Entity for Address,Inventory,Product, and orders with lombok

--- src/main/java/com/fuadrabbi/eommerce_backend/model/Inventory.java
+++ src/main/java/com/fuadrabbi/eommerce_backend/model/Inventory.java
@@ -1,10 +1,7 @@
 package com.fuadrabbi.eommerce_backend.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
-
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
