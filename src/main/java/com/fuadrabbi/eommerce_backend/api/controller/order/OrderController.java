24af65047bbde519da9db23b65406a86b4d56cac
Fuad Rabbi <fuadhrabbi66@gmail.com>
2025-06-01 14:40:36 -0400

    Made all endpoints secured by authentication by default. Put exclusions that requires authentication on specific endpoints. Tested the endpoints.

--- src/main/java/com/fuadrabbi/eommerce_backend/api/controller/order/OrderController.java
+++ src/main/java/com/fuadrabbi/eommerce_backend/api/controller/order/OrderController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RequestMapping("/order")
 public class OrderController {
     private OrderService orderService;
+
     public OrderController(OrderService orderService) {
         this.orderService = orderService;
     }
--- src/main/java/com/fuadrabbi/eommerce_backend/api/security/WebSecurityConfig.java
+++ src/main/java/com/fuadrabbi/eommerce_backend/api/security/WebSecurityConfig.java
@@ -5,14 +5,28 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
 
 @Configuration
 public class WebSecurityConfig {
+    private JWTRequestFilter jwtRequestFilter;
+
+    public WebSecurityConfig(JWTRequestFilter jwtRequestFilter) {
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
                 .cors(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
-        return http.build();
+                .addFilterBefore(jwtRequestFilter, AuthorizationFilter.class)
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/product", "/auth/register", "/auth/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .build();
     }
+
+
 }
