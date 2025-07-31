c6ec7750121174500d16937b202a0afa211de492
Fuad Rabbi <fuadhrabbi66@gmail.com>
2025-07-30 21:55:12 -0400

    fully tested LoginBody

--- src/test/java/com/fuadrabbi/eommerce_backend/service/UserServiceTest.java
+++ src/test/java/com/fuadrabbi/eommerce_backend/service/UserServiceTest.java
@@ -65,7 +65,26 @@ public class UserServiceTest {
         body.setUsername("UserA-NotExists");
         body.setPassword("PasswordA123-BadPassword");
         Assertions.assertNull(userService.loginUser(body), "The user should not exist.");
-
+        body.setUsername("UserA");
+        Assertions.assertNull(userService.loginUser(body), "The password should be incorrect.");
+        body.setPassword("PasswordA123");
+        Assertions.assertNotNull(userService.loginUser(body), "The user should login successfully.");
+        body.setUsername("UserB");
+        body.setPassword("PasswordB123");
+        try {
+            userService.loginUser(body);
+            Assertions.assertTrue(false, "User should not have email verified.");
+        } catch (UserNotVerifiedException ex) {
+            Assertions.assertTrue(ex.isNewEmailSent(), "Email verification should be sent.");
+            Assertions.assertEquals(1, greenMailExtension.getReceivedMessages().length);
+        }
+        try {
+            userService.loginUser(body);
+            Assertions.assertTrue(false, "User should not have email verified.");
+        } catch (UserNotVerifiedException ex) {
+            Assertions.assertFalse(ex.isNewEmailSent(), "Email verification should not be resent.");
+            Assertions.assertEquals(1, greenMailExtension.getReceivedMessages().length);
+        }
     }
 
 }
